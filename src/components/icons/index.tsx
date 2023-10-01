8e8d8c6b320383e6eb3ae6c11943661b31582088
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2023-10-01 01:12:17 +0100

    Add App Icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,37 @@
+import { ReactComponent as Home } from "./home.svg";
+import { ReactComponent as ChevronRight } from "./arrow-chevron-right-md.svg";
+import { ReactComponent as Calendar } from "./calendar-calendar-check.svg";
+import { ReactComponent as AndroidShare } from "./communication-share-android.svg";
+import { ReactComponent as DocumentIcon } from "./file-file-document.svg";
+import { ReactComponent as NotebookIcon } from "./file-notebook.svg";
+import { ReactComponent as HeartIcon } from "./interface-heart-01.svg";
+import { ReactComponent as UserGroupIcon } from "./user-users-group.svg";
+import { ReactComponent as SearchIcon } from "./interface-search-magnifying-glass.svg";
+import { ReactComponent as UserCheckIcon } from "./user-user-check.svg";
+import { ReactComponent as UserCloseIcon } from "./user-user-close.svg";
+import { ReactComponent as UserVoiceIcon } from "./user-user-voice.svg";
+import { ReactComponent as InfoIcon } from "./warning-circle-warning.svg";
+import { ReactComponent as TagIcon } from "./cp-icons-tag-artboard-211.svg";
+import { ReactComponent as MailIcon } from "./communication-mail.svg";
+import { ReactComponent as SettingsIcon } from "./interface-settings.svg";
+import { ReactComponent as PlayIcon } from "./media-play-circle.svg";
+
+export {
+  Home,
+  ChevronRight,
+  Calendar,
+  AndroidShare,
+  DocumentIcon,
+  NotebookIcon,
+  HeartIcon,
+  UserGroupIcon,
+  SearchIcon,
+  UserCheckIcon,
+  UserCloseIcon,
+  UserVoiceIcon,
+  InfoIcon,
+  TagIcon,
+  MailIcon,
+  SettingsIcon,
+  PlayIcon,
+};
